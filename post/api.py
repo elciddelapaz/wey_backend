750b0e8183c551fa22ff9ce2d63ebc5a92495af9
elciddelapaz <elcid.delapaz@gmail.com>
2023-08-11 10:46:24 +0800

    update: view profile and posts

--- post/api.py
+++ post/api.py
@@ -1,6 +1,8 @@
 from django.http import JsonResponse
 from .serializers import PostSerializer
 from .models import Post
+from account.models import User
+from account.serializers import UserSerializer
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from .forms import PostForm
 
@@ -14,9 +16,14 @@ def post_list(request):
 
 @api_view(['GET'])
 def post_list_profile(request, id):
+    user = User.objects.get(pk=id)
     posts = Post.objects.filter(created_by_id=id)
-    serializer = PostSerializer(posts, many=True)
-    return JsonResponse(serializer.data, safe=False)
+    post_serializer = PostSerializer(posts, many=True)
+    user_serializer = UserSerializer(user)
+    return JsonResponse({
+        'posts': post_serializer.data,
+        'user': user_serializer.data
+    }, safe=False)
 
 
 @api_view(['POST'])
