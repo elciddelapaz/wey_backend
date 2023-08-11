b5ecec2872f87a2a655989e3c97aa9d35794b224
elciddelapaz <elcid.delapaz@gmail.com>
2023-08-11 16:53:47 +0800

    update: migrations

--- post/models.py
+++ post/models.py
@@ -13,7 +13,7 @@ class PostAttachment(models.Model):
 
 class Post(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    body = models.TextField(blank=True)
+    body = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now=True)
     created_by = models.ForeignKey(
         User, related_name='posts', on_delete=models.CASCADE)
