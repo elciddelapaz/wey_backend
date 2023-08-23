967c9e225f2826c25b23b90ebdb27905f239ab16
elciddelapaz <elcid.delapaz@gmail.com>
2023-08-23 15:08:41 +0800

    update: return form errors

--- account/api.py
+++ account/api.py
@@ -33,9 +33,8 @@ def signup(request):
     if form.is_valid():
         form.save()
     else:
-        message = 'error'
-
-    return JsonResponse({'message': message})
+        message = form.errors.as_json()
+    return JsonResponse({'message': message}, safe=False)
 
 
 @api_view(['GET'])
